9c5ed53f9d6e4b46e2fe111193e642399c308b39
Andrade <38527254+JoaoAndrad@users.noreply.github.com>
2025-12-23 19:50:03 -0300

    sidebar att

--- admin-ui/src/components/sidebar/sidebar.js
+++ admin-ui/src/components/sidebar/sidebar.js
@@ -55,12 +55,9 @@ class AdminSidebar extends HTMLElement {
         <ul class="menu-list">
           <li class="menu-item"><a href="/admin/static/src/pages/Home/index.html" class="menu-link"><span class="material-icons-outlined">dashboard</span>Dashboard</a></li>
           <li class="menu-item"><a href="/admin/static/src/pages/Users/index.html" class="menu-link"><span class="material-icons-outlined">people</span>Usuários</a></li>
-          <li class="menu-item"><a href="#" id="remontarBtn" class="menu-link" title="Recriar Prisma client"><span class="material-icons-outlined">restart_alt</span>Remontar</a></li>
-          <li class="menu-item"><a href="#" id="studioBtn" class="menu-link" title="Abrir Prisma Studio"><span class="material-icons-outlined">open_in_new</span>Prisma Studio</a></li>
           <li class="menu-item"><a href="/admin/static/src/pages/Spotify/index.html" class="menu-link"><span class="material-icons-outlined">music_note</span>Spotify</a></li>
           <li class="menu-item"><a href="#" class="menu-link"><span class="material-icons-outlined">fitness_center</span>DogFort <span class="material-icons-outlined chevron">chevron_right</span></a></li>
           <li class="menu-item"><a href="#" class="menu-link"><span class="material-icons-outlined">settings</span>Configurações</a></li>
-          <li class="menu-item"><a href="#" class="menu-link"><span class="material-icons-outlined">description</span>Logs</a></li>
           <li class="menu-item"><a href="#" class="menu-link" style="align-items:flex-start"><span class="material-icons-outlined">storage</span><div style="display:flex;flex-direction:column"><span>Database Status</span><span style="font-size:0.75rem;opacity:0.7">(Prisma)</span></div><span class="material-icons-outlined chevron" style="margin-top:5px">chevron_right</span></a></li>
         </ul>
       </aside>
